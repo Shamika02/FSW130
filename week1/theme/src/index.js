aecb319bd4dc8dad819b23ee16a5b8039c9a9795
Shamika Sims <shamika.sims@bryanuniversity.edu>
2021-03-21 13:21:06 -0400

    ....

--- week1/theme/src/index.js
+++ week1/theme/src/index.js
@@ -2,18 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import App from "./App"
-import UserContext from "./userContext"
-
-/**
- * Challenge: Set up context to save the user's username and pass it to anywhere that is currently hardcoding "Username".
- * 
- * Use the static class property `contextType` on any components that need to consume context.
- * https://reactjs.org/docs/context.html
- */
+import ThemeContext from "./themeContext"
 
 ReactDOM.render(
-    <UserContext.Provider value={"sally123"}>
+    <ThemeContext.Provider value={"dark"}>
         <App />
-    </UserContext.Provider>, 
+    </ThemeContext.Provider>, 
     document.getElementById("root")
 )
