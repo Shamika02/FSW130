202ab2d5674370c90f377135f7eb12845c4ed9c9
Shamika Sims <shamika.sims@bryanuniversity.edu>
2021-03-21 12:58:35 -0400

    week1

---
+++ week1/theme/src/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+import UserContext from "./userContext"
+
+
+
+ReactDOM.render(
+    <UserContext.Provider value={"bobz123"}>
+        <App />
+    </UserContext.Provider>, 
+    document.getElementById("root")
+)
