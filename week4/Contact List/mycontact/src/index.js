be3bbdff08f9bc00ae6e6227b789ee0c7398da6a
Shamika Sims <shamika.sims@bryanuniversity.edu>
2021-04-18 10:30:51 -0400

    Update index.js

--- week4/Contact List/mycontact/src/index.js
+++ week4/Contact List/mycontact/src/index.js
@@ -1,13 +1,13 @@
 const redux = require("redux")
 
-function addContact(contact) {
+function addContact(contact = 1) {
     return {
-        type: "Add_CONTACT",
+        type: "ADD_CONTACT",
         payload: contact
     }
 }
 
-function deleteContact(contact) {
+function deleteContact(contact = 1) {
     return {
         type: "DELETE_CONTACT",
         payload: contact
@@ -17,32 +17,26 @@ function deleteContact(contact) {
 const initialState = {
     myContact:[],
     contacts: {
-
-       Name1:"Shamika Sims",
-       NumberA:"477-898-2222", 
-
-       Name2:"Honey Love",
-       NumberB:"323-125-9898",
-    
+        
+        Name:'',
+        Number:'', 
+ 
     }
 }
-
 
 function reducer(state = initialState, action) {
     switch(action.type) {
         case "ADD_CONTACT":
             return {
                 ...state,
-                contacts: action.payload,
-                myContact: [...state.myContact, action.payload]
+                contacts: [state.myContacts + action.payload]
             }
-        case "DELETE_CONTACT":{
-            const updatedArr = state.addContact.filter(contact => contact.name !== action.payload.name)
+        case "DELETE_CONTACT":
+            const updateArr = state.addContact.filter(contact => contact.name !== action.payload.name)
             return {
                 ...state,
-                myContact: updatedArr
+                myContact:updateArr
             }
-        }
         default:
             return state
     }
@@ -52,3 +46,7 @@ const store = redux.createStore(reducer)
 store.subscribe(() => {
     console.log(store.getState())
 })
+
+store.dispatch(addContact({name: 'Shamika', number:'477-898-2222'}))
+store.dispatch(addContact({name: 'Roberta', number:'111-111-1111'}))
+store.dispatch(deleteContact({name: 'Shamika', number:'477-898-2222'}))
